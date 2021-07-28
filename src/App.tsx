1d41111bbfaa528f29ee1d6b0ed7476037f982e1
96tm <khabcity@gmail.com>
2021-07-28 13:08:46 +0300

    feat: update configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader';
 
-class App extends Component {
-  render(): JSX.Element {
-    return (
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  );
 }
 
 export default hot(module)(App);
