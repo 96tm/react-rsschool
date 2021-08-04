0f45772259d973ec89623227ce10257a181c050d
96tm <khabcity@gmail.com>
2021-08-04 13:18:01 +0300

    feat: add donation block

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,6 @@ import DonationForm from './donation-form/DonationForm';
 function App() {
   return (
     <div className="App">
-      <h1 className="DonationForm__Heading">Donation</h1>
       <DonationForm />
     </div>
   );
--- src/components/donation-form/DonationForm.tsx
+++ src/components/donation-form/DonationForm.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './DonationForm.css';
+import Donation from './donation/Donation';
 
 export default function DonationForm(): JSX.Element {
   return (
     <div className="DonationForm">
       <header className="DonationForm__header">Donation</header>
+      <div className="DonationForm__container">
+        <div className="DonationForm__subheader">Donation information</div>
+        <Donation />
+      </div>
     </div>
   );
 }
